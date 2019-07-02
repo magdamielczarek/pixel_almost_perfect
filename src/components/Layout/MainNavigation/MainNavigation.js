bbe9c1c6286b153bfae8c3c8f6ceead5c6d585bb
Magda Mielczarek <mielczarek.research@gmail.com>
2019-07-02 15:05:00 +0200

    change language in main menu

--- src/components/Layout/MainNavigation/MainNavigation.js
+++ src/components/Layout/MainNavigation/MainNavigation.js
@@ -9,10 +9,10 @@ const MainNavigation = () => {
                 <h1>PixelPerfect</h1>
                 <nav className="mainNavigation">
                     <ul>
-                        <li className="mainNavigation__link"><NavLink to="/gra">Nowa gra</NavLink></li>
-                        <li className="mainNavigation__link"><NavLink to="/zasady">Zasady</NavLink></li>
-                        <li className="mainNavigation__link"><NavLink to="/ustawienia">Ustawienia</NavLink></li>
-                        <li className="mainNavigation__link"><NavLink to="/ranking">Ranking</NavLink></li>
+                        <li className="mainNavigation__link"><NavLink to="/gra">New game</NavLink></li>
+                        <li className="mainNavigation__link"><NavLink to="/zasady">Rules</NavLink></li>
+                        <li className="mainNavigation__link"><NavLink to="/ustawienia">Settings</NavLink></li>
+                        <li className="mainNavigation__link"><NavLink to="/ranking">Rank</NavLink></li>
                     </ul>
                 </nav>
             </header>

5a2492f3f20618e3989044b36fbda1e7cfb1aea2
Magda Mielczarek <mielczarek.research@gmail.com>
2019-07-02 15:06:05 +0200

    add style to intro page (without animation)

--- src/components/Pages/IntroPage/IntroPage.js
+++ src/components/Pages/IntroPage/IntroPage.js
@@ -5,7 +5,17 @@ import classes from './IntroPage.module.scss';
 
 const IntroPage = () => {
     return (
-        <div className={classes.introWrapper}>intro page</div>
+        <div className={classes.introWrapper}>
+            <div className={[classes.batPixel,classes.batPixel__1].join(' ')}> </div>
+            <div className={[classes.batPixel,classes.batPixel__2].join(' ')}> </div>
+            <div className={[classes.batPixel,classes.batPixel__3].join(' ')}> </div>
+            <div className={[classes.batPixel,classes.batPixel__4].join(' ')}> </div>
+            <div className={[classes.batPixel,classes.batPixel__5].join(' ')}> </div>
+            <div className={classes.header}>
+                <div>Pixel <span className={classes.comment}>almost</span> Perfect</div>
+            </div>
+            <button className={classes.ctaButton}>PLAY</button>
+        </div>
     );
 };
 

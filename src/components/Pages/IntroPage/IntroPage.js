7d5a52831fed2fa929947b50e8fe76e18b745d86
Magda Mielczarek <mielczarek.research@gmail.com>
2019-07-03 16:08:01 +0200

    intro page styling

--- src/components/Pages/IntroPage/IntroPage.js
+++ src/components/Pages/IntroPage/IntroPage.js
@@ -3,20 +3,20 @@ import classes from './IntroPage.module.scss';
 
 const IntroPage = () => {
     return (
-        <div className={classes.introWrapper}>
+        <header className={classes.introWrapper}>
             <div className={[classes.batPixel,classes.batPixel__1].join(' ')}> </div>
             <div className={[classes.batPixel,classes.batPixel__2].join(' ')}> </div>
             <div className={[classes.batPixel,classes.batPixel__3].join(' ')}> </div>
             <div className={[classes.batPixel,classes.batPixel__4].join(' ')}> </div>
             <div className={[classes.batPixel,classes.batPixel__5].join(' ')}> </div>
-            <div className={classes.header}>
+            <h1 className={classes.header}>
                 <span className={classes.comment}>find </span>
                 Pixels
                 <span className={classes.comment}> almost </span>
                 Perfect
-            </div>
+            </h1>
             <button className={classes.ctaButton}>START</button>
-        </div>
+        </header>
     );
 };
 

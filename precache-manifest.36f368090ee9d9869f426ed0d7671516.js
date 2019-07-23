ebedf42115e6474b5c8a1287c4ea72dcf934fba1
Magda Mielczarek <mielczarek.research@gmail.com>
2019-07-23 12:31:55 +0200

    Updates

---
+++ precache-manifest.36f368090ee9d9869f426ed0d7671516.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4416e45754b5f6bc4fef71b11cf51aed",
+    "url": "/pixel_almost_perfect/index.html"
+  },
+  {
+    "revision": "c4067589a95d660049a8",
+    "url": "/pixel_almost_perfect/static/css/main.10d0a490.chunk.css"
+  },
+  {
+    "revision": "8f0b3508d6dc069f6320",
+    "url": "/pixel_almost_perfect/static/js/2.49fda68f.chunk.js"
+  },
+  {
+    "revision": "c4067589a95d660049a8",
+    "url": "/pixel_almost_perfect/static/js/main.e7268e04.chunk.js"
+  },
+  {
+    "revision": "0bee968e66e8fcaddc84",
+    "url": "/pixel_almost_perfect/static/js/runtime~main.be929a0d.js"
+  },
+  {
+    "revision": "384c94f35235fa5cf43bec06b9710042",
+    "url": "/pixel_almost_perfect/static/media/404page.384c94f3.jpg"
+  },
+  {
+    "revision": "035eaf079c59a0bdb28e3473c34654d9",
+    "url": "/pixel_almost_perfect/static/media/bgCompressed.035eaf07.jpg"
+  },
+  {
+    "revision": "8e1ae208b5d351df48baf195c9f76991",
+    "url": "/pixel_almost_perfect/static/media/bg_image.8e1ae208.jpg"
+  },
+  {
+    "revision": "9564295b2f71a70619cdb6c8944bf4ca",
+    "url": "/pixel_almost_perfect/static/media/id10_Boticelli_Venus.9564295b.jpg"
+  },
+  {
+    "revision": "9bdec964c53534eb8b844a992b9df769",
+    "url": "/pixel_almost_perfect/static/media/id11_gustav-klimt-serpents.9bdec964.jpg"
+  },
+  {
+    "revision": "8147fbeedfef284bcdc5071501d3af26",
+    "url": "/pixel_almost_perfect/static/media/id12_Hieronymus Bosch_The Garden of Earthly Delights.8147fbee.jpg"
+  },
+  {
+    "revision": "858c1c9e17986bf61e477b6674565f55",
+    "url": "/pixel_almost_perfect/static/media/id13_Pieter Brueghel the Elder-The Dutch Proverbs.858c1c9e.jpg"
+  },
+  {
+    "revision": "2edc3f7ae923981df2b61531439a98bf",
+    "url": "/pixel_almost_perfect/static/media/id14_Winter Landscape with a Bird Trap - Pieter Brueghel The Younger.2edc3f7a.jpg"
+  },
+  {
+    "revision": "b4ad81c73fb2d978075dcdfdaa2cabf5",
+    "url": "/pixel_almost_perfect/static/media/id15_Pieter_Bruegel_de_Oude_-_De_val_van_Icarus.b4ad81c7.jpg"
+  },
+  {
+    "revision": "5b075797eb6be8f90d17fb4878666741",
+    "url": "/pixel_almost_perfect/static/media/id16_Constable_Hampstead Heath with Bathers.5b075797.jpg"
+  },
+  {
+    "revision": "c7bbe8ffecb195bae5ace1f9bbf14b1f",
+    "url": "/pixel_almost_perfect/static/media/id17_Caspar_David_Friedrich_-_Mondaufgang_am_Meer_-_Google_Art_Project.c7bbe8ff.jpg"
+  },
+  {
+    "revision": "fa3b334a6511e02065d840da0d55ecad",
+    "url": "/pixel_almost_perfect/static/media/id18_Georges_Seurat_-_A_Sunday_on_La_Grande_Jatte.fa3b334a.jpg"
+  },
+  {
+    "revision": "52dc3828729a9b2fd4c418de32f8b033",
+    "url": "/pixel_almost_perfect/static/media/id19_Georges Seurat_Baigneurs_a_Asnieres.52dc3828.jpg"
+  },
+  {
+    "revision": "67edda7ef9d40f4a5301381eb75c5b87",
+    "url": "/pixel_almost_perfect/static/media/id1_Tamara_Lempicka_Autoportret_w_zielonym_bugatti.67edda7e.png"
+  },
+  {
+    "revision": "1e2ac98e89c1b5d4271707946617e892",
+    "url": "/pixel_almost_perfect/static/media/id20_Pierre-Auguste_Renoir_-_Luncheon_of_the_Boating_Party.1e2ac98e.jpg"
+  },
+  {
+    "revision": "81c6601b51cd4f90ffae8c299f9a68f5",
+    "url": "/pixel_almost_perfect/static/media/id21_-Pierre-Auguste_Renoir,_Le_Moulin_de_la_Galette.81c6601b.jpg"
+  },
+  {
+    "revision": "ba77b69ce4a33f0df829018fa9592cd2",
+    "url": "/pixel_almost_perfect/static/media/id22_Rembrandt_The stone bridge.ba77b69c.png"
+  },
+  {
+    "revision": "8802990ac510291f8e07df738a6f7672",
+    "url": "/pixel_almost_perfect/static/media/id23_Stuart Davis_The Mallow Pad.8802990a.png"
+  },
+  {
+    "revision": "dfccf0b3b41cba8d894ad24daa5d272c",
+    "url": "/pixel_almost_perfect/static/media/id24_The Outcry - Wolfgang Lettl.dfccf0b3.jpg"
+  },
+  {
+    "revision": "a1294fda73bff7bd973999a10bb05e73",
+    "url": "/pixel_almost_perfect/static/media/id25_Andy Warhol_Self portrait.a1294fda.png"
+  },
+  {
+    "revision": "e526d43b461474344ae29a1f4dbec41e",
+    "url": "/pixel_almost_perfect/static/media/id26_Edward Munch_Madonna.e526d43b.jpg"
+  },
+  {
+    "revision": "37ad3f4e44ff8d89f9c1a9c789280c5a",
+    "url": "/pixel_almost_perfect/static/media/id27_the-dance-of-life-by-edvard-munch.37ad3f4e.jpg"
+  },
+  {
+    "revision": "611f1b8176abaa3533ee6cb9247e0a0d",
+    "url": "/pixel_almost_perfect/static/media/id28_Peter_Paul_Rubens_Sad Parysa.611f1b81.jpg"
+  },
+  {
+    "revision": "3d112fb55c8e9c735f926974d94fcfcf",
+    "url": "/pixel_almost_perfect/static/media/id29_pablo-picasso-guernica.3d112fb5.jpg"
+  },
+  {
+    "revision": "95b8ce4af7b412eaab075b7781f45b6e",
+    "url": "/pixel_almost_perfect/static/media/id2_Vincent_van_Gogh_-_Wheatfield_with_crows.95b8ce4a.jpg"
+  },
+  {
+    "revision": "e9c38442493f3aa0af0593470bd95a7e",
+    "url": "/pixel_almost_perfect/static/media/id30_Edgar Degas, The Rehearsal of the Ballet Onstage.e9c38442.jpg"
+  },
+  {
+    "revision": "c4cecdbfccefa79255e56925dbe62242",
+    "url": "/pixel_almost_perfect/static/media/id31_malczewski-hamlet-polski.c4cecdbf.jpg"
+  },
+  {
+    "revision": "19fe764f08eb0e38e9df244e655142ad",
+    "url": "/pixel_almost_perfect/static/media/id32_Jan_Matejko,_Bitwa_pod_Grunwaldem.19fe764f.jpg"
+  },
+  {
+    "revision": "a27398e8d4a1f01d3979cd3292244cf4",
+    "url": "/pixel_almost_perfect/static/media/id33_Creation_of_Adam_Articles_Michelangelo_Adam.a27398e8.jpg"
+  },
+  {
+    "revision": "1c275f6c74faf1267bd511245986156b",
+    "url": "/pixel_almost_perfect/static/media/id34_Leonardo_Da_Vinci_-_Annunciazione.1c275f6c.jpeg"
+  },
+  {
+    "revision": "e48a31898eed1dc6c8408b9fbecf6129",
+    "url": "/pixel_almost_perfect/static/media/id35_Wham_Roy Lichtenstein.e48a3189.jpg"
+  },
+  {
+    "revision": "aa3b03cdd91ab5d001b895e1cf6f1e76",
+    "url": "/pixel_almost_perfect/static/media/id36_keith_haring_ignorance_fear_1989.aa3b03cd.jpg"
+  },
+  {
+    "revision": "bd8ce0f7e90d0972371221ca44ad87ef",
+    "url": "/pixel_almost_perfect/static/media/id37_Strzeminski_Pejzaz lodzki.bd8ce0f7.jpg"
+  },
+  {
+    "revision": "011967c46e15de2991f16a9133df1ba4",
+    "url": "/pixel_almost_perfect/static/media/id38_Cyclist_Goncharova.011967c4.jpg"
+  },
+  {
+    "revision": "875be99e3c2cc08499f0a84a94bd8b2b",
+    "url": "/pixel_almost_perfect/static/media/id39_Dwurnik_Krakowskie Przedmiescie.875be99e.jpg"
+  },
+  {
+    "revision": "68c26ab0ccc5f5e8e76ec04d58fbdc55",
+    "url": "/pixel_almost_perfect/static/media/id3_Bernardo_Bellotto_il_Canaletto_-_New_Market_Square_in_Dresden_from_the_Judenhof.68c26ab0.jpg"
+  },
+  {
+    "revision": "e2fde7427fd5f6feb787c17f630cdbe1",
+    "url": "/pixel_almost_perfect/static/media/id40_Warszawa Turystyczna Tytus Brzozowski 2018.e2fde742.jpg"
+  },
+  {
+    "revision": "ede2e5f1fbcb28f255075c0428b46494",
+    "url": "/pixel_almost_perfect/static/media/id41_Tytus Brzozowski Lodz.ede2e5f1.jpg"
+  },
+  {
+    "revision": "66a24bc49123e8d40efbf81e70df03d1",
+    "url": "/pixel_almost_perfect/static/media/id42_Kamienice na poniatowszczaku Tytus Brzozowski.66a24bc4.jpg"
+  },
+  {
+    "revision": "1d54a40cf678482eee2fa41b81cf3841",
+    "url": "/pixel_almost_perfect/static/media/id43_Hokusai_Great_Wave_off_Kanagawa2.1d54a40c.jpg"
+  },
+  {
+    "revision": "5001e6275a73aa28fbb27436ffb4fae1",
+    "url": "/pixel_almost_perfect/static/media/id44_Katsushika_Hokusai_-_Fine_Wind_Clear_Morning.5001e627.jpg"
+  },
+  {
+    "revision": "b1ff002a79f08858154b9bf9742a5d1b",
+    "url": "/pixel_almost_perfect/static/media/id45_Peter_Paul_Rubens_-_A_View_of_Het_Steen_in_the_Early_Morning.b1ff002a.jpg"
+  },
+  {
+    "revision": "9ad87cd529485d0d7bf3e15037e86c2c",
+    "url": "/pixel_almost_perfect/static/media/id46_John_Constable_-_Wivenhoe_Park.9ad87cd5.jpg"
+  },
+  {
+    "revision": "6cc39ce3c682cf1dac02ea062551d2bb",
+    "url": "/pixel_almost_perfect/static/media/id47_Jackson Pollock_Blue Poles.6cc39ce3.png"
+  },
+  {
+    "revision": "9c5f4f13119854637ad6cd7efe29285e",
+    "url": "/pixel_almost_perfect/static/media/id48_Malczewski_melancholia.9c5f4f13.jpg"
+  },
+  {
+    "revision": "47424e2b3204963cfde158d88ea53f0c",
+    "url": "/pixel_almost_perfect/static/media/id49_Tamara Lempicka Lato 1928.47424e2b.jpg"
+  },
+  {
+    "revision": "daf06d4c6ee9dbe42af4e6b9f94d20c5",
+    "url": "/pixel_almost_perfect/static/media/id4_Vermeer Johannes_Woman reading list.daf06d4c.jpg"
+  },
+  {
+    "revision": "7e6787bf1df4fe3c9a2b4c4cfc82f58e",
+    "url": "/pixel_almost_perfect/static/media/id50_Witkacy_Kuszenie_sw_A_2.7e6787bf.jpg"
+  },
+  {
+    "revision": "1d5e52e46fe801fc7b4cccf3073c3462",
+    "url": "/pixel_almost_perfect/static/media/id51_The two Fridas frida_kahlo.1d5e52e4.jpg"
+  },
+  {
+    "revision": "756c50823a86f34a08dfe94409936ead",
+    "url": "/pixel_almost_perfect/static/media/id5_Vincent_van_Gogh_-_Wheatfield_Under_Thunderclouds.756c5082.jpg"
+  },
+  {
+    "revision": "e0aaa15eeba1c9011aa68e69bde08604",
+    "url": "/pixel_almost_perfect/static/media/id6_Henri_Matisse,_1904,_Luxe,_Calme_et_Volupté.e0aaa15e.jpg"
+  },
+  {
+    "revision": "07e82123ab7d68b86cb0584750f934a0",
+    "url": "/pixel_almost_perfect/static/media/id7_Leonardo da Vinci_L'Ultima Cena.07e82123.jpg"
+  },
+  {
+    "revision": "89f25faf1cd3379b782410eac1fbcad7",
+    "url": "/pixel_almost_perfect/static/media/id8_Morbeli AngeloThe valley of Usseglio (1918).89f25faf.jpg"
+  },
+  {
+    "revision": "45b171c8a73e307972c99e6139aa3661",
+    "url": "/pixel_almost_perfect/static/media/id9_Angelo Morbelli_Eighty cents 1895.45b171c8.jpg"
+  }
+]);
